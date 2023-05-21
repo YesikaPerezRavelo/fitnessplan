90ac34997bf815e6625f3b93b828ac77cab560e1
Yesika Perez Ravelo <yesikapr@gmail.com>
2023-05-21 14:38:10 -0300

    organinzando carrito

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 function crearCliente(nombre, apellido) {
-  this.nombre = nombre = prompt("Ingresa tu nombre");
-  this.apellido = apellido = prompt("Ingresa tu apellido");
+  this.nombre = nombre || prompt("Ingresa tu nombre");
+  this.apellido = apellido || prompt("Ingresa tu apellido");
   this.saludarCliente = function () {
     return `¡Hola ${this.nombre}, ${this.apellido}!`;
   };
 }
+
 const cliente1 = new crearCliente();
 alert(cliente1.saludarCliente());
 
 let edad = prompt("Ingresa tu edad");
 if (edad >= 18) {
-  alert("Eres mayor de edad puedes continuar");
+  alert("Eres mayor de edad, puedes continuar");
 } else {
-  alert("para continuar necesitas un adulto mayor");
+  alert("Para continuar necesitas un adulto mayor");
   edad = prompt("Ingrese la edad del responsable");
   while (edad <= 18) {
     edad = prompt(
-      "La persona responsable, debe ser mayor de edad, escribir OK"
+      "La persona responsable debe ser mayor de edad. Ingresa la edad del responsable:"
     );
   }
 }
 
 function datosIMC() {
-  let peso = prompt(`Ingresa tu peso`);
-  let altura = prompt(`Ingresa tu estatura en metros EJEMPLO: 1.63`);
+  let peso = prompt("Ingresa tu peso");
+  let altura = prompt("Ingresa tu estatura en metros (EJEMPLO: 1.63)");
   let alCuadrado = altura * altura;
-
   let IMC = peso / alCuadrado;
 
   alert("Calcularemos tu índice de Masa Corporal, escribe OK para continuar");
@@ -33,41 +33,39 @@ function datosIMC() {
   if (IMC < 18.5) {
     alert("Estás por debajo del peso apropiado");
   } else if (IMC >= 18.5 && IMC <= 24.9) {
-    alert("Sos saludable");
+    alert("Eres saludable");
   } else if (IMC >= 25 && IMC <= 29.9) {
-    alert("Tenés sobrepeso");
+    alert("Tienes sobrepeso");
   } else if (IMC >= 30 && IMC <= 34.9) {
-    alert("Tenés obesidad tipo 1");
+    alert("Tienes obesidad tipo 1");
   } else if (IMC >= 35 && IMC <= 39.9) {
-    alert("Tenés obesidad tipo 2");
+    alert("Tienes obesidad tipo 2");
   } else if (IMC > 40) {
-    alert("Tenés obesidad tipo 3");
+    alert("Tienes obesidad tipo 3");
   } else {
-    alert("Puede que no ingresaste un dato, o ingresaste un dato inválido");
+    alert("Puede que no ingresaste un dato o ingresaste un dato inválido");
   }
 }
+
 datosIMC();
 
-let email = prompt(`Ingresa un email`);
-let newPassword = prompt(`Ingresa un password`);
+let email = prompt("Ingresa un email");
+let newPassword = prompt("Ingresa una contraseña");
 
-if (email != "" && newPassword != "") {
+if (email !== "" && newPassword !== "") {
   let confirmation = "1) " + email + " " + "2) " + newPassword + " ";
 
   console.log(confirmation);
 } else {
-  alert("Opción invalidad falta cargar datos");
+  alert("Opción inválida, falta cargar datos");
 }
 
 function registro() {
-  let repeatPassword = prompt(`Confirma el password`);
-  if (newPassword == repeatPassword) {
-    alert("Password correcto");
+  let repeatPassword = prompt("Confirma la contraseña");
+  if (newPassword === repeatPassword) {
+    alert("Contraseña correcta");
   } else {
-    alert("Password incorrecto");
-  }
-
-  for (let i = 0; i < 0; i++) {
+    alert("Contraseña incorrecta");
     registro();
   }
 }
@@ -103,7 +101,7 @@ function cargarProducto() {
       break;
 
     case "3":
-      alert("Miercoles");
+      alert("Miércoles");
       break;
 
     case "4":
@@ -115,7 +113,7 @@ function cargarProducto() {
       break;
 
     case "6":
-      alert("Sabado");
+      alert("Sábado");
       break;
 
     case "7":
@@ -123,14 +121,13 @@ function cargarProducto() {
       break;
 
     default:
-      alert("Opción invalidad");
-
+      alert("Opción inválida");
       break;
   }
-  alert("Valor por hora" + precios);
+  alert("Valor por hora: " + precios);
   const nuevoProducto = new LetsWorkout(dias, precios);
   arrayCarrito.push(nuevoProducto);
-  alert("Has añadido día de entrenamiento a tu carrito");
+  alert("Has añadido un día de entrenamiento a tu carrito");
 }
 
 function descuentoExclusivo() {
@@ -139,24 +136,26 @@ function descuentoExclusivo() {
   );
 
   let primerProducto = Number(
-    prompt("¿Cuantas veces por MES te gustaria entrenar de manera presencial?")
+    prompt("¿Cuántas veces por MES te gustaría entrenar de manera presencial?")
   );
   let segundoProducto = Number(
-    prompt("¿Cuantas clases por MES en linea te gustaria tomar?")
+    prompt("¿Cuántas clases por MES en línea te gustaría tomar?")
   );
   let suma = primerProducto + segundoProducto;
 
   if (suma >= 1 && suma <= 11) {
-    alert("Descuento aplica para más cantidad de clases");
+    alert("El descuento aplica para una mayor cantidad de clases");
   } else if (suma >= 12 && suma <= 26) {
-    alert("Tendras un 15% de descuento sobre el valor total de tus clases");
+    alert("Tendrás un 15% de descuento sobre el valor total de tus clases");
   } else {
-    alert("Opción invalidad");
+    alert("Opción inválida");
   }
   const nuevoDescuento = new descuentoExclusivo();
   arrayCarrito.push(nuevoDescuento);
-  alert("Descuento" + descuentoExclusivo);
+  alert("Descuento: " + descuentoExclusivo);
 }
+
+let reservaHorariaCount = 0;
 
 function reservaHoraria() {
   class Agenda {
@@ -175,27 +174,33 @@ function reservaHoraria() {
   const agenda1 = new Agenda(
     "Lunes",
     "Martes",
-    "Miercoles",
+    "Miércoles",
     "Jueves",
     "Viernes",
-    "Sabado",
+    "Sábado",
     "Domingo"
   );
-  for (let index = 0; index < 3; index++) {
+  for (let index = 1; index <= 3; index++) {
     let entrada = parseInt(
       prompt(
-        "Ingresa un horario que te gustria entrenar con nosotros y te diremos si tenemos disponibilidad horaria. Horario Militar, ejemplo 16"
+        "Ingresa un horario en el que te gustaría entrenar con nosotros y te diremos si tenemos disponibilidad horaria. Horario Militar, ejemplo 16"
       )
     );
     if (agenda1.estaDisponible(entrada)) {
-      alert("Este horario esta disponible " + entrada);
+      alert("Este horario está disponible: " + entrada);
     } else {
-      alert("Este horario no esta disponible " + entrada);
+      alert("Este horario no está disponible: " + entrada);
     }
   }
-  const nuevoHorario = new reservaHoraria();
-  arrayCarrito.push(nuevoHorario);
-  alert("Descuento" + reservaHoraria);
+
+  reservaHorariaCount++;
+  if (reservaHorariaCount >= 3) {
+    alert("Ya has realizado el máximo de reservas horarias permitidas");
+  } else {
+    const nuevoHorario = new reservaHoraria();
+    arrayCarrito.push(nuevoHorario);
+    alert("Descuento: " + reservaHoraria);
+  }
 }
 
 function verCarrito() {
@@ -205,21 +210,19 @@ function verCarrito() {
 }
 
 function mostrarOferta() {
-  alert("Te presentamos nuestro produtos");
+  alert("Te presentamos nuestros productos");
 
-  const { libro, precio, disponible } = producto;
-
-  function producto(nombre, precio) {
+  function Producto(nombre, precio) {
     (this.libro = nombre), (this.precio = precio), (this.disponible = true);
   }
 
-  const libro1 = new producto("Abdomen de hierro", 8000);
-  const libro2 = new producto("Tren superior para la postura", 9000);
-  const libro3 = new producto(
-    "Mas de 100 entrenamientos para hacer desde casa",
+  const libro1 = new Producto("Abdomen de hierro", 8000);
+  const libro2 = new Producto("Tren superior para la postura", 9000);
+  const libro3 = new Producto(
+    "Más de 100 entrenamientos para hacer desde casa",
     10000
   );
-  const libro4 = new producto(
+  const libro4 = new Producto(
     "Los mejores entrenamientos para armar una linda cola y piernas sexy",
     15000
   );
@@ -229,17 +232,19 @@ function mostrarOferta() {
   alert(libro3.libro + " " + libro3.precio);
   alert(libro4.libro + " " + libro4.precio);
 
-  const ofertas = precio.filter((libro) => libro.precio < 9000);
+  const ofertas = [libro1, libro2, libro3, libro4].filter(
+    (libro) => libro.precio < 9000
+  );
   for (const oferta of ofertas) {
     alert(
-      `la oferta de hoy es: ${oferta.libro} a un valor de ${oferta.precio}`
+      `La oferta de hoy es: ${oferta.libro} a un valor de ${oferta.precio}`
     );
   }
 }
 
 function finalizarCompra() {
-  const total = arrayCarrito.reduce((acc, el) => acc + el.precio, 0);
-  alert(`gracias por su compra el total a pagar es de ${total}`);
+  const total = arrayCarrito.reduce((acc, el) => acc + el.precios, 0);
+  alert(`Gracias por tu compra. El total a pagar es de ${total}`);
 }
 
 let arrayCarrito = [];
@@ -271,4 +276,4 @@ while (opcion !== "7") {
     "Bienvenido a Yes Fitness \n 1: Elegir otro día de entrenamiento \n 2: Mostrar ofertas \n 3: Participar de descuentos exclusivos \n 4: Ver disponibilidad horaria \n 5: Ver carrito \n 6: Finalizar compra \n 7: Salir"
   );
 }
-alert("gracias por tu compra!");
+alert("¡Gracias por tu compra!");
